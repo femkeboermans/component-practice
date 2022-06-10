9d5b30bc3013bddd6b08b49cef1f8429404a6d0e
Femke Boermans <femkeboermans@MacBook-Air-van-Femke.local>
2022-06-10 13:33:13 +0200

    dynamic components toegevoegd

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,15 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <h2>Scoped Slots</h2>
     <course-goal #default="slotProps">
       <h2>{{ slotProps.item }}</h2>
       <p>{{ slotProps.anotherProp }}</p>
     </course-goal>
+    <h2>Dynamic Components</h2>
+    <button @click="selectedCmp('goal-new')">New Goal</button>
+    <button @click="selectedCmp('goal-old')">Old Goal</button>
+    <component :is="selectedComponent"></component>
   </div>
 </template>
 
@@ -19,6 +24,8 @@ import TheHeader from "./components/TheHeader.vue";
 import BadgeList from "./components/BadgeList.vue";
 import UserInfo from "./components/UserInfo.vue";
 import CourseGoal from "./components/CourseGoal.vue";
+import GoalNew from "./components/GoalNew.vue";
+import GoalOld from "./components/GoalOld.vue";
 
 export default {
   components: {
@@ -26,6 +33,8 @@ export default {
     BadgeList,
     UserInfo,
     CourseGoal,
+    GoalNew,
+    GoalOld,
   },
   data() {
     return {
@@ -34,7 +43,13 @@ export default {
         description: "Site owner and admin",
         role: "admin",
       },
+      selectedComponent: "goal-new",
     };
+  },
+  methods: {
+    selectedCmp(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
---
+++ src/components/GoalOld.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>Old Goal</h2>
+</template>

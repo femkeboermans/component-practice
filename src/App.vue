fd8c3e219aece30b80f1c07a1bda62aa2b74fac1
Femke Boermans <femkeboermans@MacBook-Air-van-Femke.local>
2022-06-10 11:06:34 +0200

    componenten van global naar local

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,22 @@
 </template>
 
 <script>
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+
 export default {
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo,
+  },
   data() {
     return {
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Femke Boermans",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
   },
